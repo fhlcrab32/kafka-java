861cbde5b9bbffc5b680ab93431e1326764a36db
sonyjtp <sonyjtp@gmail.com>
2024-03-24 15:00:10 -0500

    Added album topic

--- src/main/java/com/learn/kafka/Main.java
+++ src/main/java/com/learn/kafka/Main.java
@@ -2,9 +2,11 @@ package com.learn.kafka;
 
 import com.learn.kafka.config.KafkaConfig;
 import com.learn.kafka.config.Topic;
+import com.learn.kafka.consumer.AlbumConsumer;
 import com.learn.kafka.consumer.Consumer;
-import com.learn.kafka.consumer.LogConsumer;
 import com.learn.kafka.consumer.StateDataConsumer;
+import com.learn.kafka.model.Album;
+import com.learn.kafka.producer.AlbumProducer;
 import com.learn.kafka.producer.LogProducer;
 import com.learn.kafka.producer.Producer;
 import com.learn.kafka.producer.StateDataProducer;
@@ -21,24 +23,28 @@ public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
-    private static final ExecutorService executorService = Executors.newFixedThreadPool(4);
+    private static final ExecutorService executorService = Executors.newFixedThreadPool(6);
 
     public static void main(String[] args) {
         KafkaConfig config = KafkaConfig.getInstance();
-        executorService.execute( () -> consumeStateData(config));
-        executorService.execute(() -> produceStateData(config));
-        executorService.execute( () ->  new LogConsumer(config, Topic.CONNECT).receive());
-        executorService.execute(() -> {
-                try {
-                    Producer<Double, String> producer = new LogProducer(config, Topic.CONNECT);
-                    Thread.sleep(1000);
-                    log.info("Sending messages to topic {}", Topic.SIMPLE);
-                    producer.getSourceData().forEach(producer::send);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-        });
+//        executorService.execute(() -> consumeStateData(config));
+//        executorService.execute(() -> produceStateData(config));
+//        executorService.execute(() ->  new LogConsumer(config, Topic.CONNECT_STANDALONE).receive());
+//        executorService.execute(() -> produceLogs(config));
+        executorService.execute(() ->  new AlbumConsumer(config, Topic.CONNECT_DISTRIBUTED).receive());
+        executorService.execute(() -> produceAlbumData(config));
         shutdownTasks();
+    }
+
+    private static void produceAlbumData(KafkaConfig config) {
+        try {
+            Producer<Double, Album> producer = new AlbumProducer(config, Topic.CONNECT_DISTRIBUTED);
+            Thread.sleep(1000);
+            log.info("Sending messages to topic {}", Topic.CONNECT_DISTRIBUTED);
+            producer.getSourceData().forEach(producer::send);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void consumeStateData(KafkaConfig config) {
@@ -57,10 +63,21 @@ public class Main {
         }
     }
 
+    private static void produceLogs(KafkaConfig config) {
+        try {
+            Producer<Double, String> producer = new LogProducer(config, Topic.CONNECT_STANDALONE);
+            Thread.sleep(1000);
+            log.info("Sending messages to topic {}", Topic.CONNECT_STANDALONE);
+            producer.getSourceData().forEach(producer::send);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static void shutdownTasks() {
         try {
             executorService.shutdown();
-            boolean completed = executorService.awaitTermination(60, TimeUnit.SECONDS);
+            boolean completed = executorService.awaitTermination(120, TimeUnit.SECONDS);
             if(!completed) {
                 List<Runnable> incompleteTasks = executorService.shutdownNow();
                 if (!incompleteTasks.isEmpty()) {
@@ -72,5 +89,4 @@ public class Main {
             log.error("InterruptedException thrown while shutting down", e);
         }
     }
-
 }
--- src/main/java/com/learn/kafka/config/KafkaConfigFactory.java
+++ src/main/java/com/learn/kafka/config/KafkaConfigFactory.java
@@ -1,5 +1,8 @@
 package com.learn.kafka.config;
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.DoubleDeserializer;
@@ -19,11 +22,19 @@ public class KafkaConfigFactory {
                 properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                         DoubleSerializer.class.getName());
             }
-            case CONNECT -> {
+            case CONNECT_STANDALONE -> {
                 properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                         DoubleSerializer.class.getName());
                 properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                         StringSerializer.class.getName());
+            }
+            case CONNECT_DISTRIBUTED ->   {
+                properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                        DoubleSerializer.class.getName());
+                properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                        KafkaAvroSerializer.class.getName());
+                properties.setProperty(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                        "http://localhost:8081");
             }
         }
     }
@@ -36,12 +47,20 @@ public class KafkaConfigFactory {
                 properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                         DoubleDeserializer.class.getName());
             }
-           case CONNECT -> {
+           case CONNECT_STANDALONE -> {
                 properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                         DoubleDeserializer.class.getName());
                 properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                         StringDeserializer.class.getName());
             }
+           case CONNECT_DISTRIBUTED ->   {
+               properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                       DoubleDeserializer.class.getName());
+               properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                       KafkaAvroDeserializer.class.getName());
+               properties.setProperty(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                       "http://localhost:8081");
+           }
         }
     }
 }
